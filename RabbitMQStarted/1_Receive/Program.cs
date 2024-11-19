90fa73d6e03cd881b28e3cd570c5a59be15b7f62
Juan Stevan <cjuanstevan@gmail.com>
2024-11-19 17:02:24 -0500

    Simple Queue and Consumer Ack

---
+++ RabbitMQStarted/1_Receive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello", false, false, false, null);
+
+Console.WriteLine("[] Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Receive message {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: true, consumer);
+
+Console.WriteLine("Press Enter for exit");
+Console.Read();
---
+++ RabbitMQStarted/1_Send/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello", false, false, false, null);
+
+string? message = null;
+
+do
+{
+    Console.WriteLine("Please enter your message");
+    message = Console.ReadLine();
+    if (!string.IsNullOrEmpty(message))
+    {
+        SendMessage(channel, message);
+    }
+
+} while (!string.IsNullOrEmpty(message));
+
+async void SendMessage(IChannel channel, string message)
+{
+    var body = Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(string.Empty, "hello", body);
+    Console.WriteLine($"[x] Send {message}");
+}
